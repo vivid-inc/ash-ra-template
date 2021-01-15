1c981175bf4cafc58c79560ac698d7c4f5db9455
ty <ty@vivid-inc.net>
2021-01-15 17:33:28 +0900

    Re-merged bin/gen\* scripts into one bash-based script that is more readable than it ever has been.

--- bin/gen.sh
+++ bin/gen.sh
@@ -1,13 +1,46 @@
 #!/usr/bin/env bash
+# Copyright 2021 Vivid Inc.
+
+# This tool is meant to be used from the project root, as so:
+#     $ bin/gen.clj
 
 set -o errexit
 set -o xtrace
 
+function bootstrap_art {
+  clojure -Sdeps '{:deps {vivid/ash-ra-template {:mvn/version "0.5.0"
+                                                 :local/root  "art"}
+                          zprint {:mvn/version "1.0.2"}}}' - <<EOS
+
+(require '[vivid.art])
+
+(def ^:const vivid-art-version "0.5.0")
+
+(defn rndr [from to opts]
+  (as-> (slurp from) c
+        (vivid.art/render c opts)
+        (spit to c)))
+
+(rndr "art/assets/project.clj.art" "art/project.clj"
+      {:bindings     {'vivid-art-version vivid-art-version}
+       :dependencies {'zprint {:mvn/version "1.0.2"}}})
+
+(rndr "art/assets/README.md.art" "art/README.md"
+      {:bindings     {'vivid-art-version vivid-art-version}
+       :delimiters   {:begin-forms "{%"
+                      :end-forms   "%}"
+                      :begin-eval  "{%="
+                      :end-eval    "%}"}
+       :dependencies {'vivid/art {:mvn/version "0.5.0"}}})
+
+EOS
+}
+
 echo Generating resources in all projects
 
-# Project files. Install the ART .jar into Maven's repository, making
-# it available to subsequent build steps that depend on it.
-bin/gen-art.clj
+# Generate art/ project files, then install the ART .jar into the local Maven
+# repository, making it available to subsequent build steps that depend on it.
+bootstrap_art
 (cd art && lein install)
 
 (cd boot-art && boot lein-generate mkdocs)
