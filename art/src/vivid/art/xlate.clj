c15b6cdf208027f47de832c52a2d47f6ec2e9a69
ty <ty@vivid-inc.net>
2023-11-05 00:18:09 +0900

    Replaced reduce-fsm with metosin/tilakone.core. All dependencies are for the shipping lib are now free of Clojure-specific deps.

--- art/src/vivid/art/xlate.clj
+++ art/src/vivid/art/xlate.clj
@@ -16,45 +16,52 @@
   (:refer-clojure :exclude [eval])
   (:require
    [clojure.string]
-   [reduce-fsm :as fsm]))
+   [tilakone.core :as tk]))
 
 (defn echo
   "Writes an (emit) to the compiled code that echoes the plain string but with escaping."
-  [acc val & _]
+  [acc val]
   (let [escaped (clojure.string/escape val {\" "\\\""
                                             \\ "\\\\"})]
-    (update-in acc [:output] conj (str "(emit \"" escaped "\")"))))
+    (conj acc (str "(emit \"" escaped "\")"))))
 
 (defn eval
   "Writes an (emit) to the compiled code that outputs the result of evaluating the forms."
-  [acc expr & _]
-  (update-in acc [:output] conj (str "(emit " expr " )")))
+  [acc expr]
+  (conj acc (str "(emit " expr " )")))
 
 (defn forms
   "Echoes Clojure forms from the template to the compiled code."
-  [acc expr & _]
-  (update-in acc [:output] conj expr))
+  [acc expr]
+  (conj acc expr))
 
-; TODO Explicitly test all combinations (DFA state transitions), in part to clarify the rules.
-(fsm/defsm lenient-fsm
-  [[:echo
-    :vivid.art/begin-eval -> :eval
-    :vivid.art/begin-forms -> :forms
-    :vivid.art/end-eval -> :echo
-    :vivid.art/end-forms -> :echo
-    _ -> {:action echo} :echo]
-   [:eval
-    :vivid.art/end-eval -> :echo
-    :vivid.art/end-forms -> :echo
-    _ -> {:action eval} :eval]
-   [:forms
-    :vivid.art/end-eval -> :echo
-    :vivid.art/end-forms -> :echo
-    _ -> {:action forms} :forms]]
-  :default-acc {:output []})
+(def lenient-fsm-states
+  "Lenient in that this FSM will accept :end-* tokens while already in the end state of :echo."
+  [{::tk/name :echo
+    ::tk/transitions [{::tk/on :vivid.art/begin-eval   ::tk/to :eval}
+                      {::tk/on :vivid.art/begin-forms  ::tk/to :forms}
+                      {::tk/on :vivid.art/end-eval     ::tk/to :echo}
+                      {::tk/on :vivid.art/end-forms    ::tk/to :echo}
+                      {::tk/on ::tk/_                  ::tk/actions [echo]}]}
+   {::tk/name :eval
+    ::tk/transitions [{::tk/on :vivid.art/end-eval     ::tk/to :echo}
+                      {::tk/on :vivid.art/end-forms    ::tk/to :echo}
+                      {::tk/on ::tk/_                  ::tk/actions [eval]}]}
+   {::tk/name :forms
+    ::tk/transitions [{::tk/on :vivid.art/end-eval     ::tk/to :echo}
+                      {::tk/on :vivid.art/end-forms    ::tk/to :echo}
+                      {::tk/on ::tk/_                  ::tk/actions [forms]}]}])
+
+(def lenient-fsm
+  {::tk/states lenient-fsm-states
+   ::tk/action! (fn [{::tk/keys [action signal] :as fsm}]
+                  (update-in fsm [::tk/process :output] action signal))
+   ::tk/state   :echo
+   :output      []})
 
 (defn translate
   "Translates a sequence of tokens into Clojure code that,
   when evaluated, produces the template output."
   [token-stream]
-  (:output (lenient-fsm token-stream)))
+  (let [result (reduce tk/apply-signal lenient-fsm token-stream)]
+      (:output result)))
